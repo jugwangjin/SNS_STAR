dbb63f663c359dd336dc8ecddd3eb080793b9112
github desktop <gwangjin@postech.ac.kr>
2019-07-04 00:57:01 +0900

    apply feedback

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -14,6 +14,7 @@ var youtubeReady = false;
 
 var targets = [];
 var checkInt;
+var buttonchosen = false;
 var buttonVisible = true;
 
 var timeslider = $("#timeslider");
@@ -24,38 +25,39 @@ function onYouTubeIframeAPIReady() {
     youtubeReady = true;
 }
 function initiallizeVideo(){
+    $("#greeting").hide();
+    makeCurrent(1);
     // $("#greeting").fadeOut(function(){
         // makeCurrent(1);
     // });
-console.log("H");
-    var waitForYoutubeReady = setInterval(function(){
-        if (youtubeReady==true){
-            $("#currentvideo").remove();
+// console.log("H");
+//     var waitForYoutubeReady = setInterval(function(){
+//         if (youtubeReady==true){
+//             $("#currentvideo").remove();
 
-            $("#choice1video").remove();
-            $("#choice2video").remove();
-            // $("#videoholder").append("<div id='choice1video' class='choicevideos'></div>");
-            // player = new YT.Player('choice1video', {
-            $("#videoholder").append("<div id='currentvideo'></div>");
-            player = new YT.Player('currentvideo', {
-                height: '1080',
-                videoId: flow[firstvideonum].videoid,
-                events: {
-                    'onReady': onPlayerReady,
-                    'onStateChange': onPlayerStateChange
-                },
-                playerVars: {'controls': 1, 'fs': 0, 'showinfo': 0, 'rel': 0, 'playsinline': 1, 'modestbranding': 1, 'iv_load_policy': 3}
-            });
-            // choice1videonum = firstvideonum;
-            currentvideonum = firstvideonum;
+//             $("#choice1video").remove();
+//             $("#choice2video").remove();
+//             // $("#videoholder").append("<div id='choice1video' class='choicevideos'></div>");
+//             // player = new YT.Player('choice1video', {
+//             $("#videoholder").append("<div id='currentvideo'></div>");
+//             player = new YT.Player('currentvideo', {
+//                 height: '1080',
+//                 videoId: flow[firstvideonum].videoid,
+//                 events: {
+//                     'onReady': onPlayerReady,
+//                     'onStateChange': onPlayerStateChange
+//                 },
+//                 playerVars: {'controls': 0, 'fs': 0, 'showinfo': 0, 'rel': 0, 'playsinline': 1, 'modestbranding': 1, 'iv_load_policy': 3, 'disablekb': 1}
+//             });
+//             // choice1videonum = firstvideonum;
+//             currentvideonum = firstvideonum;
 
-            if(flow[firstvideonum].isEnding == false)
-                waitForCurrentReady();
-            loadChoices(currentvideonum);
-            clearInterval(waitForYoutubeReady);
-        }
-    }, 100);   
-    $("#greeting").hide();
+//             if(flow[firstvideonum].isEnding == false)
+//                 waitForCurrentReady();
+//             loadChoices(currentvideonum);
+//             clearInterval(waitForYoutubeReady);
+//         }
+//     }, 100);   
 }
 function initiallizePage(){
     $("#greeting").show();
@@ -63,7 +65,12 @@ function initiallizePage(){
     $("#currentvideo").remove();
     $("#choice1video").remove();
     $("#choice2video").remove();
- 
+    var waitForYoutubeReady = setInterval(function(){
+        if (youtubeReady==true){
+            clearInterval(waitForYoutubeReady);
+            loadChoices(-1);
+        }
+    }, 100);  
 }
 
 function checkPlayTime(){
@@ -78,7 +85,7 @@ function checkPlayTime(){
                 showButtons();
                 timeslider.width(((timeToChangeVideo-curtime)*10)+'%');
                 if(curtime >= timeToChangeVideo){
-                    makeCurrent(1);
+                    makeCurrent(choicenum);
                     clearInterval(checkInt); 
                 }
             }else{
@@ -90,7 +97,7 @@ function checkPlayTime(){
         checkInt = setInterval(function(){
             var curtime = currenttarget.getCurrentTime();
             if(curtime >= timeToChangeVideo){
-                    makeCurrent(1);
+                    makeCurrent(choicenum);
                     clearInterval(checkInt); 
             }
         }, 500);
@@ -108,7 +115,7 @@ function waitForCurrentReady(){
 }
 
 function showButtons(){
-    if(buttonVisible == false){
+    if(buttonVisible == false && buttonchosen == false){
         timeslider.width('100%');
         $("#buttonsholder").fadeIn();
         buttonVisible = true;
@@ -124,6 +131,8 @@ function hideButtons(){
 
 function onPlayerReady(event){ 
     targets[event.target.i.id] = event.target;
+    // event.target.setPlaybackQuality('hd720');
+    // event.target.setPlaybackQuality('720p');
     event.target.setPlaybackQuality('hd1080');
     event.target.setPlaybackQuality('1080p');
     event.target.playVideo();
@@ -133,9 +142,22 @@ function onPlayerStateChange(event){
     if(event.data == -1){
         event.target.playVideo();
     }
+    else if(event.data == YT.PlayerState.PLAYING){
+        if(event.target != targets['currentvideo']){
+            // event.target.setPlaybackQuality('hd720');
+            // event.target.setPlaybackQuality('720p');
+            event.target.setPlaybackQuality('hd1080');
+            event.target.setPlaybackQuality('1080p');
+            event.target.pauseVideo();
+        }
+    }
     else if(event.data == 3){
         if(event.target != targets['currentvideo']){
-            event.target.pauseVideo();
+            // event.target.setPlaybackQuality('hd720');
+            // event.target.setPlaybackQuality('720p');
+            event.target.setPlaybackQuality('hd1080');
+            event.target.setPlaybackQuality('1080p');
+            // event.target.pauseVideo();
         }
         else{
             event.target.playVideo();
@@ -202,57 +224,97 @@ function makeCurrent(choicenum){
 }
 
 function loadChoices(num){
-    if(flow[num].isEnding == true){
-        return;
+    if(num==-1) { 
+        choice1videonum = firstvideonum;
+        choice2videonum = firstvideonum;
     }
-    var choiceelem;
-    choiceelem = $("#choice1video");
-    if(choiceelem != undefined){
-        choiceelem.remove();
-    }
-    choiceelem = $("#choice2video");
-    if(choiceelem != undefined){
-        choiceelem.remove();
+    else{
+        if(flow[num].isEnding == true){
+            return;
+        }
+        var choiceelem;
+        choiceelem = $("#choice1video");
+        if(choiceelem != undefined){
+            choiceelem.remove();
+        }
+        choiceelem = $("#choice2video");
+        if(choiceelem != undefined){
+            choiceelem.remove();
+        }
+
+        choice1videonum = flow[currentvideonum].choice1;
+        choice2videonum = flow[currentvideonum].choice2;
     }
 
-    choice1videonum = flow[currentvideonum].choice1;
-    choice2videonum = flow[currentvideonum].choice2;
+    buttonchosen = false;
+    choicenum = 1;
 
     $("#videoholder").append("<div id='choice1video' class='choicevideos'></div>");
-    $("#choicebutton1").text(flow[num].choice1text);
     $("#videoholder").append("<div id='choice2video' class='choicevideos'></div>");
-    $("#choicebutton2").text(flow[num].choice2text);
-
-    player = new YT.Player('choice1video', {
-        height: '1080',
-        // width: '0',
-        videoId: flow[flow[num].choice1].videoid,
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        },
-        playerVars: {'controls': 1, 'fs': 0, 'showinfo': 0, 'rel': 0, 'playsinline': 1, 'modestbranding': 1, 'iv_load_policy': 3}
-    });
-    player = new YT.Player('choice2video', {
-        height: '1080',
-        // width: '0',
-        videoId: flow[flow[num].choice2].videoid,
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        },
-        playerVars: {'controls': 1, 'fs': 0, 'showinfo': 0, 'rel': 0, 'playsinline': 1, 'modestbranding': 1, 'iv_load_policy': 3}
-    });
+    if(num != -1){
+        $("#choicebutton1").text(flow[num].choice1text);
+        $("#choicebutton2").text(flow[num].choice2text);
+    }
+    if(num == -1){
+        player = new YT.Player('choice1video', {
+            height: '1080',
+            // width: '0',
+            videoId: flow[firstvideonum].videoid,
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            },
+            playerVars: {'controls': 1, 'fs': 0, 'showinfo': 0, 'rel': 0, 'playsinline': 1, 'modestbranding': 1, 'iv_load_policy': 3, 'disablekb': 1}
+        });
+        player = new YT.Player('choice2video', {
+            height: '1080',
+            // width: '0',
+            videoId: flow[firstvideonum].videoid,
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            },
+            playerVars: {'controls': 1, 'fs': 0, 'showinfo': 0, 'rel': 0, 'playsinline': 1, 'modestbranding': 1, 'iv_load_policy': 3, 'disablekb': 1}
+        });
+    }
+    else{
+        player = new YT.Player('choice1video', {
+            height: '1080',
+            // width: '0',
+            videoId: flow[flow[num].choice1].videoid,
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            },
+            playerVars: {'controls': 1, 'fs': 0, 'showinfo': 0, 'rel': 0, 'playsinline': 1, 'modestbranding': 1, 'iv_load_policy': 3, 'disablekb': 1}
+        });
+        player = new YT.Player('choice2video', {
+            height: '1080',
+            // width: '0',
+            videoId: flow[flow[num].choice2].videoid,
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            },
+            playerVars: {'controls': 1, 'fs': 0, 'showinfo': 0, 'rel': 0, 'playsinline': 1, 'modestbranding': 1, 'iv_load_policy': 3, 'disablekb': 1}
+        });
+    }   
 }
 
 function choiceButtonClick(event){
     if (event.target.id == 'choicebutton1'){
-        makeCurrent(1);
-        clearInterval(checkInt);
+        choicenum = 1;
+        buttonchosen = true;
+        hideButtons();
+        // makeCurrent(1);
+        // clearInterval(checkInt);
     }
     else if(event.target.id == 'choicebutton2'){
-        makeCurrent(2);
-        clearInterval(checkInt);
+        choicenum = 2;
+        buttonchosen = true;
+        hideButtons();
+        // makeCurrent(2);
+        // clearInterval(checkInt);
     }
 }
 
@@ -283,10 +345,10 @@ for (var i=0; i<choicebuttons.length; i++){
 $("#startbutton")[0].addEventListener('click', initiallizeVideo);
 $("#restartbutton")[0].addEventListener('click', initiallizePage);
 $("#fullscreenbutton")[0].addEventListener('click', toggleFullscreen);
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    $("#mobilenotice").show();
-}
-else{
-    $("#mobilenotice").hide();
-}
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+//     $("#mobilenotice").show();
+// }
+// else{
+//     $("#mobilenotice").hide();
+// }
 initiallizePage();
